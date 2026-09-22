37b60b9ef6fdd287d4cd86dda9efd603c80ec6d3
BryanPool99 <bryanpool991@hotmail.com>
2026-09-21 20:07:29 -0500

    first commit

---
+++ src/main/java/com/bpao/devfoliobuilderapi/DevfolioBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.bpao.devfoliobuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfolioBuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfolioBuilderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpao/devfoliobuilderapi/DevfolioBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpao.devfoliobuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfolioBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
